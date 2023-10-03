2a929acd27981eb46b26d6e3c352693627d8320d
Mintemir Kurbanaliev <kurbanaliev.mintemir@gmail.com>
2023-10-03 22:40:02 +0600

    added tag

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "interview-project-dev-bucket-12345dsfgds"
     key    = "terraform/dev/"
-    region = "us-east-1"
+    region = "us-east-12"
   }
 }
 #hello from tag
